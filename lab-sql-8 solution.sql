59467b14a4a0c2f8672ac888e354afbe6e8b97d8
Joana BV <joanaviana95@gmail.com>
2021-11-02 10:59:12 +0000

    lab solution w q7

--- lab-sql-8 solution.sql
+++ lab-sql-8 solution.sql
@@ -57,20 +57,48 @@ ORDER BY GrossRevenue DESC
 LIMIT 5;
 
 -- Is "Academy Dinosaur" available for rent from Store 1?
-SELECT s.store_id, f.title, COUNT(i.inventory_id) AS items
+SELECT s.store_id, f.title,
+CASE
+WHEN COUNT(i.store_id) > 0 THEN 'YES'
+ELSE 'NO'
+END AS available_to_rent, COUNT(i.store_id) AS inventory
 FROM sakila.store s
 JOIN sakila.inventory i USING (store_id)
 JOIN sakila.film f USING (film_id)
 JOIN sakila.rental r USING (inventory_id)
-GROUP BY s.store_id
-HAVING f.title = 'Academy Dinosaur'
-ORDER BY s.store_id ASC
-LIMIT 1;
+WHERE (s.store_id = '1') AND (r.return_date IS NOT NULL) AND (f.title = 'Academy Dinosaur');
 
 -- Get all pairs of actors that worked together.
-
+SELECT  a.first_name, a.last_name, f.title AS MovieTheyCoStarredIn
+FROM sakila.actor a
+RIGHT JOIN sakila.film_actor fa
+USING(actor_id)
+LEFT JOIN sakila.film f
+USING(film_id);
 
 -- Get all pairs of customers that have rented the same film more than 3 times.
+-- SELECT c1.customer_id AS Client1, c2.customer_id AS Client2, c1.first_name AS Client1_FirstName, c1.last_name AS Client1_LastName, c2.first_name AS Client2_FirstName, c2.last_name AS Client2_LastName, i.film_id, COUNT(i.film_id) AS times_rented
+-- FROM sakila.rental  r
+-- LEFT JOIN sakila.inventory i
+-- USING (inventory_id)
+-- LEFT JOIN sakila.customer c1
+-- USING (customer_id)
+-- INNER JOIN sakila.customer c2
+-- ON (c1.customer_id <> c2.customer_id);
 
+-- SELECT r.customer_id AS client_1,r1.customer_id AS client_2, f.title, COUNT(i.film_id) AS times_rented_by_customer
+-- FROM sakila.rental r
+-- INNER JOIN sakila.inventory i
+-- ON r.inventory_id = i.inventory_id
+-- INNER JOIN sakila.rental r1
+-- ON (r.customer_id <> r1.customer_id)
+-- INNER JOIN sakila.inventory i1
+-- ON r1.inventory_id = i1.inventory_id
+-- INNER JOIN sakila.film f
+-- ON i.film_id = f.film_id
+-- WHERE i.film_id = i1.film_id
+-- GROUP BY r.customer_id,r1.customer_id,f.title
+-- HAVING COUNT(i.film_id) >= 3 AND COUNT(i1.film_id) >= 3
+-- ORDER BY r.customer_id ASC;
 
 -- For each film, list actor that has acted in more films.
